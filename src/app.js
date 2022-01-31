1a490b522d25c5d917dd5ba2498d4ada331a5fa4
Saario91 <huasmg1@gmail.com>
2022-01-31 10:59:18 +0100

    Integrated API and automated time display

---
+++ src/app.js
@@ -0,0 +1,66 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${days[day]} ${hours}:${minutes}`;
+}
+function currentWeather(response) {
+  // Update the HTML with new data
+  let tempElem = document.querySelector("#temp-display");
+  let windElem = document.querySelector("#wind-display");
+  let humidElem = document.querySelector("#humid-display");
+  let descripElem = document.querySelector("#descrip-display");
+  let nameElem = document.querySelector("#city-name");
+  let dateElem = document.querySelector("#date-display");
+
+  tempElem.innerHTML = Math.round(response.data.main.temp);
+  windElem.innerHTML = Math.round(response.data.wind.speed);
+  humidElem.innerHTML = Math.round(response.data.main.humidity);
+  descripElem.innerHTML = response.data.weather[0].description;
+  nameElem.innerHTML = response.data.name;
+  dateElem.innerHTML = formatDate(response.data.dt * 1000);
+
+  // Update date and time
+  new Date(response.data.dt * 1000);
+}
+// declarin API variables
+let APIkey = "421760f2fa0cfa886ced8b96269374ed";
+let unit = "metric";
+let cityName = "Rotterdam";
+let APIurl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&appid=${APIkey}`;
+
+// Declaring time variables
+let months = [
+  "January",
+  "Februari",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+axios.get(APIurl).then(currentWeather);
